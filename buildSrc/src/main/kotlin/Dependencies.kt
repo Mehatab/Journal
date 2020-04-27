0da6b54a8a2df0279939e968822eff5759b6f479
Mehatab <shaikh.mehatab@gmail.com>
2020-04-27 18:36:58 +0530

    - Bug fixes and improvements

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2020 Mehatab Shaikh.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.gradle.api.JavaVersion
 
 object Config {
-    val applicationId = "com.spendy"
+    val applicationId = "com.journal"
     val minSdk = 21
     val compileSdk = 29
     val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
     val buildTools = "29.0.3"
-    val versionCode = 9
-    val versionName = "1.0.2"
+    val versionCode = 1
+    val versionName = "1.0.0"
 }
 
 object Versions {
